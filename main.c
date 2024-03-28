6303c0f45f6a02d029710bcb586cf9039de3ec82
Joseba Jon Eceiza Belaunzaran <j.eceiza@opendeusto.es>
2024-03-28 17:55:52 +0100

    Panel Inicial

--- main.c
+++ main.c
@@ -1,5 +1,38 @@
 #include<stdio.h>
 
 void main(){
-    printf("Hola mundo");
+    printf("BIENVENIDO A HOTEL 33");
+    printf("\n1. Iniciar Sesion");
+    printf("\n2. Registrar Usuario");
+    printf("\n3. Salir del programa");
+
+    int ve = 1;
+    if (ve == 1){
+        //INICIAR SESION
+        printf("1. Realizar reserva");
+        printf("2. Anular reserva");
+        printf("3. Historial de reservas por cliente (DNI)");
+        printf("4. Consultar datos habitaciones");
+        printf("5. Consultar datos usuario");
+        printf("6. Cerrar sesion");
+
+        
+
+
+
+    }
+    else if (ve == 2){
+        //REGISTRAR USUARIO
+        printf("Introduzca su nombre: "):
+        printf("Introduzca su apellido: "):
+        printf("Introduzca su DNI: "):
+        printf("Introduzca su contraseña: "):
+
+
+    }
+
+    else if (ve == 3){
+        //SALIR DEL PROGRAMA
+
+    }
 }
