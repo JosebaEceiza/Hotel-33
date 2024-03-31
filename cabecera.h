f01ba38279616d220702f4288bf71177c84e143e
Joseba Jon Eceiza Belaunzaran <j.eceiza@opendeusto.es>
2024-03-31 22:00:36 +0200

    Estructuras

--- cabecera.h
+++ cabecera.h
@@ -1,6 +1,45 @@
 #ifndef cabecera_h
 #define cabecera_h
 
+typedef struct{
+    int dia;
+    int mes;
+    int anyo;
+} Fecha;
+
+
+typedef struct{
+    char *DNI;
+    char *nombre;
+    char apellido;
+    int telefono;
+    int num_tarjeta
+    Fecha fecha_nac;
+} Cliente;
+
+typedef struct{
+    int id_tipo_comida;
+    char *nombre;
+    char *descripcion;
+} Tipo_comida;
+
+typedef struct{
+    int id_tipo_habitacion;
+    char *nombre;
+    char *descripcion;
+} Tipo_jabitacion;
+
+typedef struct{
+    int id_habitacion;
+    int id_tipo_habitacion;
+} Habitacion;
+
+typedef struct{
+    int id_reserva_hotel;
+    char *DNI;
+    int id_habitacion;
+}
+
 void crearBD();
 
 
